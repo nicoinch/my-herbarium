1cbfc3acd214396b760f64d6aa9df20967747745
Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
2023-09-17 13:07:42 +0200

    :bug: Fix transparent colors tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   content: [
     join(
       __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      '{src,pages,components,app,.storybook}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
@@ -17,17 +17,17 @@ module.exports = {
         dark: '#151515',
         light: '#ffffff',
         'pink-dark': '#640227',
-        'pink-dark-10': '#640227',
+        'pink-dark-10': '#efe6e9',
         'pink-medium': '#833552',
         'pink-light': '#f8d9e8',
         'pink-highlight': '#ff62ae',
         'blue-dark': '#1f2c2e',
-        'blue-dark-10': '#1f2c2e',
+        'blue-dark-10': '#e9eaea',
         'blue-medium': '#354143',
         'blue-light': '#9cb5ba',
         'blue-highlight': '#9beeff',
         'green-dark': '#394107',
-        'green-dark-10': '#394107',
+        'green-dark-10': '#ebece5',
         'green-medium': '#4d5420',
         'green-light': '#d4e99c',
         'green-highlight': '#97c910',
