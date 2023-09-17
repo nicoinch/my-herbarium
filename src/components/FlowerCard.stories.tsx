562cc3eda7b573f263819aa78413685189ea9005
Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
2023-09-17 19:29:19 +0200

    :sparkles: Add FlowerCard component

---
+++ src/components/FlowerCard.stories.tsx
@@ -0,0 +1,98 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { FlowerCard } from './FlowerCard';
+import { darkModeDecorator } from '../../.storybook/dark-mode-decorator';
+
+const Story: Meta<typeof FlowerCard> = {
+  component: FlowerCard,
+  title: 'Card',
+  decorators: [
+    (Story) => (
+      <div className="p-4 flex flex-col gap-3">
+        <Story />
+      </div>
+    ),
+    darkModeDecorator,
+  ],
+};
+
+export default Story;
+
+export const Basic: StoryObj<typeof FlowerCard> = {
+  name: '🧰 Basic',
+  args: {
+    color: 'pink',
+    title: 'Gerbera',
+    temperature: '12.3',
+    humidity: '10.3',
+    description:
+      'Gerbera is a genus of plants in the Asteraceae (Compositae) family. Gerbera species are tufted, caulescent, perennial herbs, often with woolly crown, up to 80 cm high.',
+    backgroundUrl: './img/gerbera.png',
+  },
+};
+
+export const VariantColor: StoryObj<typeof FlowerCard> = {
+  name: '🎭 Variant - Color',
+  render: () => (
+    <div className="flex flex-col gap-3">
+      <FlowerCard
+        color="pink"
+        title="Tulip"
+        temperature="12.3"
+        humidity="10.3"
+        description="Tulips (Tulipa) are a genus of spring-blooming perennial herbaceous bulbiferous geophytes. The flowers are usually large, showy and brightly colored, generally red, pink, yellow, or white."
+        backgroundUrl="./img/tulip.png"
+        lightValue={23}
+        waterValue={39}
+      />
+      <FlowerCard
+        color="blue"
+        title="Gerbera"
+        temperature="12.3"
+        humidity="10.3"
+        description="Gerbera is a genus of plants in the Asteraceae (Compositae) family. Gerbera species are tufted, caulescent, perennial herbs, often with woolly crown, up to 80 cm high."
+        backgroundUrl="./img/gerbera.png"
+        lightValue={63}
+        waterValue={79}
+      />
+      <FlowerCard
+        color="green"
+        title="Zinnia"
+        temperature="12.3"
+        humidity="10.3"
+        description="Zinnias are annuals which flower officially in summer, but with the number of cultivars available you're covered for spring into autumn."
+        backgroundUrl="./img/zinnia.png"
+        lightValue={16}
+        waterValue={87}
+      />
+    </div>
+  ),
+};
+
+export const StateOpen: StoryObj<typeof FlowerCard> = {
+  name: '🔁 State - Open',
+  render: () => (
+    <div className="flex flex-col gap-3">
+      <FlowerCard
+        color="pink"
+        title="Tulip"
+        temperature="12.3"
+        humidity="10.3"
+        description="Tulips (Tulipa) are a genus of spring-blooming perennial herbaceous bulbiferous geophytes. The flowers are usually large, showy and brightly colored, generally red, pink, yellow, or white."
+        backgroundUrl="./img/tulip.png"
+        lightValue={23}
+        waterValue={39}
+      />
+      <FlowerCard
+        color="pink"
+        title="Tulip"
+        temperature="12.3"
+        humidity="10.3"
+        description="Tulips (Tulipa) are a genus of spring-blooming perennial herbaceous bulbiferous geophytes. The flowers are usually large, showy and brightly colored, generally red, pink, yellow, or white."
+        backgroundUrl="./img/tulip.png"
+        lightValue={23}
+        waterValue={39}
+        open
+      />
+    </div>
+  ),
+};
---
+++ src/components/FlowerCard.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import clsx from 'clsx';
+import { Color } from 'src/types/color';
+import { Badge } from './Badge';
+import { ThermometerIcon } from './icons/Thermometer';
+import { HumidityIcon } from './icons/Humidity';
+import { Slider } from './Slider';
+import { DropletIcon } from './icons/Droplet';
+import { SunIcon } from './icons/Sun';
+
+type FlowerCardProps = {
+  open?: boolean;
+  title: string;
+  temperature: string;
+  humidity: string;
+  description: string;
+  color?: Color;
+  backgroundUrl?: string;
+  lightValue?: number;
+  waterValue?: number;
+};
+
+export const FlowerCard: React.FC<FlowerCardProps> = ({
+  open = false,
+  title,
+  temperature,
+  description,
+  humidity,
+  color = 'blue',
+  backgroundUrl,
+  lightValue,
+  waterValue,
+}) => {
+  const [openState, setOpenState] = React.useState(open);
+  const classes = [];
+  const descriptionClasses = [];
+
+  switch (color) {
+    case 'pink':
+      classes.push('bg-pink-light text-pink-dark');
+      classes.push('dark:bg-pink-dark dark:text-pink-light');
+      descriptionClasses.push('bg-pink-dark text-pink-light');
+      descriptionClasses.push('dark:bg-pink-light dark:text-pink-dark');
+      break;
+    case 'blue':
+      classes.push('bg-blue-light text-blue-dark');
+      classes.push('dark:bg-blue-dark dark:text-blue-light');
+      descriptionClasses.push('bg-blue-dark text-blue-light');
+      descriptionClasses.push('dark:bg-blue-light dark:text-blue-dark');
+      break;
+    case 'green':
+      classes.push('bg-green-light text-green-dark');
+      classes.push('dark:bg-green-dark dark:text-green-light');
+      descriptionClasses.push('bg-green-dark text-green-light');
+      descriptionClasses.push('dark:bg-green-light dark:text-green-dark');
+      break;
+  }
+
+  // List flowers background images to force tailwind detect them
+  // bg-[url('./img/chrysanthemum.png')]
+  // bg-[url('./img/gerbera.png')]
+  // bg-[url('./img/orchid.png')]
+  // bg-[url('./img/tulip.png')]
+  // bg-[url('./img/zinnia.png')]
+  return (
+    <div className={clsx(`rounded-2xl overflow-hidden`, ...classes)}>
+      <div
+        className={`flex flex-col gap-3 p-6 py-[29px] bg-[url('${backgroundUrl}')] bg-no-repeat bg-right-bottom bg-contain cursor-pointer`}
+        onClick={() => {
+          setOpenState(!openState);
+        }}
+      >
+        <div className="text-title-2 font-semibold">{title}</div>
+        <div className="flex gap-1">
+          <Badge
+            label={temperature + '°C'}
+            icon={<ThermometerIcon />}
+            color={color}
+          />
+          <Badge label={humidity + '%'} icon={<HumidityIcon />} color={color} />
+        </div>
+      </div>
+      {openState ? (
+        <div>
+          {description ? (
+            <div className={clsx(descriptionClasses, 'px-6 py-4')}>
+              <div className="text-text">{description}</div>
+            </div>
+          ) : null}
+          <div className="flex flex-col gap-2 px-3 py-4">
+            <Slider
+              startIcon={<SunIcon crossedOut />}
+              endIcon={<SunIcon />}
+              label="Light"
+              color={color}
+              value={lightValue}
+            />
+            <Slider
+              startIcon={<DropletIcon crossedOut />}
+              endIcon={<DropletIcon />}
+              label="Water"
+              color={color}
+              value={waterValue}
+            />
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
