6d92d9a054d542326ca75f2e0324ddb599d9b35e
Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
2023-09-18 13:44:12 +0200

    :children_crossing: Improve Slider usability

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import clsx from 'clsx';
 import * as RadixSlider from '@radix-ui/react-slider';
+import * as RadixPopover from '@radix-ui/react-popover';
 import { Color } from 'src/types/color';
 
 type SliderProps = {
@@ -27,6 +28,8 @@ export const Slider: React.FC<SliderProps> = ({
   step = 1,
   onChange,
 }) => {
+  const [popoverOpen, setPopoverOpen] = React.useState(false);
+  const [sliderValue, setSliderValue] = React.useState(value);
   const classesTrack: Array<string> = [];
   const classesRange: Array<string> = [];
   const classesIcon: Array<string> = [];
@@ -58,6 +61,21 @@ export const Slider: React.FC<SliderProps> = ({
       break;
   }
 
+  const handleMouseEnter = () => {
+    setPopoverOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setPopoverOpen(false);
+  };
+
+  const handleOnChange = (value: Array<number>) => {
+    if (onChange) {
+      onChange(value);
+    }
+    setSliderValue(value[0]);
+  };
+
   return (
     <div className="flex gap-2 p-1 items-center justify-center">
       <div className={clsx('w-6', classesIcon)}>{startIcon}</div>
@@ -67,7 +85,7 @@ export const Slider: React.FC<SliderProps> = ({
         min={min}
         max={max}
         step={step}
-        onValueChange={onChange}
+        onValueChange={handleOnChange}
       >
         <RadixSlider.Track
           className={clsx('h-[2px] rounded-full relative grow', classesTrack)}
@@ -84,7 +102,21 @@ export const Slider: React.FC<SliderProps> = ({
             boxShadow: '2px 2px 4px 0px rgba(0, 0, 0, 0.15)',
           }}
           aria-label={label}
-        />
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <RadixPopover.Root open={popoverOpen}>
+            <RadixPopover.Trigger></RadixPopover.Trigger>
+            <RadixPopover.Content
+              className="w-10 bg-white text-dark focus:outline-none shadow rounded-md p-2 translate-x-3"
+              side="top"
+              sideOffset={20}
+            >
+              <div className="w-full text-text text-center">{sliderValue}</div>
+              <RadixPopover.Arrow className="fill-light" />
+            </RadixPopover.Content>
+          </RadixPopover.Root>
+        </RadixSlider.Thumb>
       </RadixSlider.Root>
       <div className={clsx('w-6', classesIcon)}>{endIcon}</div>
     </div>
